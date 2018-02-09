a715e07581d9841cfbf736255d6a38da7e5bb81d
DSerov <dserov@gmail.com>
2018-02-09 12:23:16 +0300

    Более, менее законченная игра

--- src/main/java/GameLogic.java
+++ src/main/java/GameLogic.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * игровая логика
  *
@@ -5,53 +7,40 @@
  * @version dated 07 feb, 2018
  */
 public class GameLogic {
-    private char DOT_X = 'X';
-    private char DOT_O = 'O';
-    private char DOT_EMPTY = '.';
+    private final char DOT_X = 'X';
+    private final char DOT_O = 'O';
+    private final char DOT_EMPTY = '.';
+    private final char DOT_MASK = ' ';
 
     /**
      * массив игрового поля
      */
-    private int map[][];
+    private ArrayList<Character> board;
 
     /**
      * сиволы, которыми играет игрок
      */
     private char dotUser;
+
     /**
      * символы, которыми играет ИИ
      */
     private char dotAi;
+
     /**
-     * ширина игрового поля
+     * размер игрового поля
      */
-    private int fieldWidth;
-    /**
-     * высота игрового поля
-     */
-    private int fieldHeight;
+    private int fieldSize;
+
     /**
      * длина выигрышной игровой последовательности
      */
     private int winSequenceLength;
 
-    public void setFieldWidth(int fieldWidth) {
-        this.fieldWidth = fieldWidth;
-    }
-
-    public void setFieldHeight(int fieldHeight) {
-        this.fieldHeight = fieldHeight;
-    }
-
-    public void setWinSequenceLength(int winSequenceLength) {
+    GameLogic(int fieldSize, int winSequenceLength) throws Exception {
+        this.fieldSize = fieldSize;
         this.winSequenceLength = winSequenceLength;
-    }
-
-    public GameLogic(int fieldWidth, int fieldHeight, int winSequenceLength) {
-        this.fieldWidth = fieldWidth;
-        this.fieldHeight = fieldHeight;
-        this.winSequenceLength = winSequenceLength;
-        initMap();
+        initBoard();
     }
 
     public void setDotUser(char dotUser) {
@@ -65,13 +54,356 @@ public class GameLogic {
     /**
      * проинициализируем игровое поле умолчательными значениями
      */
-    private void initMap() {
-        for (int i = 0; i < fieldHeight; i++)
-            for (int j = 0; j < fieldWidth; j++)
-                map[i][j] = DOT_EMPTY;
+    private void initBoard() throws Exception {
+//        char[] test =
+//                {DOT_X, DOT_O, DOT_EMPTY, DOT_EMPTY,
+//                        DOT_X, DOT_EMPTY, DOT_EMPTY, DOT_O,
+//                        DOT_EMPTY, DOT_EMPTY, DOT_EMPTY, DOT_EMPTY,
+//                        DOT_O, DOT_O, DOT_EMPTY, DOT_EMPTY};
+
+        if (fieldSize == 0)
+            throw new Exception("Неверные параметры инициализации игрового поля");
+        board = new ArrayList<>(fieldSize * fieldSize);
+        for (int i = 0; i < fieldSize * fieldSize; i++)
+            board.add(DOT_EMPTY);
+//            board.add(test[i]);
     }
 
-    public int[][] getMap() {
-        return map.clone();
+    /**
+     * FIXME: тут бы возвращать копию объекта, чтоб его не изменили снаружи класса
+     *
+     * @return ArrayList
+     */
+    public ArrayList<Character> getBoard() {
+        return board;
     }
+
+    public int getFieldSize() {
+        return fieldSize;
+    }
+
+    public int getFieldHeight() {
+        return fieldSize;
+    }
+
+    /**
+     * проверка, что ячейка пуста
+     *
+     * @param x int
+     * @param y int
+     * @return boolean
+     */
+    private boolean isCellEmpty(int x, int y) {
+        return (x >= 0 || y >= 0 || x < fieldSize || y < fieldSize) &&
+                (board.get(y * fieldSize + x).equals(DOT_EMPTY));
+    }
+
+
+    public boolean makeHumanTurn(int x, int y) {
+        if (isCellEmpty(x, y)) {
+            board.set(y * fieldSize + x, dotUser);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Выиграл юзер?
+     *
+     * @return boolean
+     */
+    public boolean isPlayerWin() {
+        return (isWinCombination(board, dotUser));
+    }
+
+    /**
+     * Выиграл ИИ?
+     *
+     * @return boolean
+     */
+    public boolean isAiWin() {
+        return (isWinCombination(board, dotAi));
+    }
+
+    /**
+     * проверка, что указанный символ - выиграл
+     *
+     * @param board расклад, для которого идет расчет
+     * @param dot   char игрок или ии
+     * @return boolean true - выиграл
+     */
+    private boolean isWinCombination(ArrayList<Character> board, char dot) {
+        for (int i = 0; i < board.size(); i++) {
+            if (isWinHorizontal(board, i, dot) ||
+                    isWinVertical(board, i, dot) ||
+                    isWinDiagonal(board, i, dot) ||
+                    isWinAntiDiagonal(board, i, dot))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Ищем выигрышную обратную диагональ
+     *
+     * @param board расклад, для которого идет расчет
+     * @param index int стартовая точка
+     * @param dot   char искомый символ
+     * @return boolean
+     */
+    private boolean isWinAntiDiagonal(ArrayList<Character> board, int index, char dot) {
+        int x = index % fieldSize;
+        int y = index / fieldSize;
+        if (x < (winSequenceLength - 1) || y > (fieldSize - winSequenceLength))
+            return false; // выигрышная антидиагональ не существует
+        // есть шанс
+        for (int i = 0; i < winSequenceLength; i++) {
+            if (board.get(index) == dot) {
+                index += (fieldSize - 1);
+            } else
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Ищем выигрышную прямую диагональ
+     *
+     * @param board расклад, для которого идет расчет
+     * @param index int стартовая точка
+     * @param dot   char искомый символ
+     * @return boolean
+     */
+    private boolean isWinDiagonal(ArrayList<Character> board, int index, char dot) {
+        int x = index % fieldSize;
+        int y = index / fieldSize;
+        if (x > (fieldSize - winSequenceLength) || y > (fieldSize - winSequenceLength))
+            return false; // выигрышная диагональ не существует
+        // есть шанс
+        for (int i = 0; i < winSequenceLength; i++) {
+            if (board.get(index) == dot) {
+                index += (fieldSize + 1);
+            } else
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Ищем выигрышную горизонталь
+     *
+     * @param board расклад, для которого идет расчет
+     * @param index int стартовая точка
+     * @param dot   char искомый символ
+     * @return boolean
+     */
+    private boolean isWinHorizontal(ArrayList<Character> board, int index, char dot) {
+        int x = index % fieldSize;
+        if (x > (fieldSize - winSequenceLength))
+            return false; // выигрышная горизонталь не существует
+        // есть шанс
+        for (int i = 0; i < winSequenceLength; i++) {
+            if (board.get(index) == dot) {
+                index++;
+            } else
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Ищем выигрышную вертикаль
+     *
+     * @param board расклад, для которого идет расчет
+     * @param index int стартовая точка
+     * @param dot   char искомый символ
+     * @return boolean
+     */
+    private boolean isWinVertical(ArrayList<Character> board, int index, char dot) {
+        int y = index / fieldSize;
+        if (y > (fieldSize - winSequenceLength))
+            return false; // выигрышная вертикаль не существует
+        // есть шанс
+        for (int i = 0; i < winSequenceLength; i++) {
+            if (board.get(index) == dot) {
+                index += fieldSize;
+            } else
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Проверка, заполнена ли доска?
+     *
+     * @return boolean
+     */
+    public boolean isBoardFilled() {
+        for (Character cell : board) {
+            if (cell == DOT_EMPTY) return false;
+        }
+        return true;
+    }
+
+    /**
+     * расчет хода ИИ
+     */
+    public void makeAiTurn() {
+        Move move;
+        if (fieldSize < 4)
+            move = MiniMax(board, dotAi);
+        else {
+            // попытка сократить число обрабатываемых ячеек
+            ArrayList<Integer> list;
+            list = maskFreeCells();
+            move = MiniMax(board, dotAi);
+            unmaskFreeCells(list);
+        }
+        board.set(move.index, dotAi);
+    }
+
+    /**
+     * хранит индексы пустых клеток, рассматривать которые нет необходимости
+     *
+     * @return ArrayList
+     */
+    private ArrayList<Integer> maskFreeCells() {
+        ArrayList<Integer> list = new ArrayList<>();
+        int x, y, x1, y1;
+        boolean found;
+        int maxIndex = board.size();
+        for (int i = 0; i < maxIndex; i++) {
+            if (board.get(i) != DOT_EMPTY) continue;
+            found = true;
+            x = i % fieldSize;
+            y = i / fieldSize;
+            x1 = x - 1;
+            y1 = y - 1;
+            if (x1 >= 0 && x1 < fieldSize && y1 >= 0 && y1 < fieldSize)
+                found &= (board.get(y1 * fieldSize + x1) == DOT_EMPTY);
+            x1 = x;
+            y1 = y - 1;
+            if (x1 >= 0 && x1 < fieldSize && y1 >= 0 && y1 < fieldSize)
+                found &= (board.get(y1 * fieldSize + x1) == DOT_EMPTY);
+            x1 = x + 1;
+            y1 = y - 1;
+            if (x1 >= 0 && x1 < fieldSize && y1 >= 0 && y1 < fieldSize)
+                found &= (board.get(y1 * fieldSize + x1) == DOT_EMPTY);
+            x1 = x + 1;
+            y1 = y;
+            if (x1 >= 0 && x1 < fieldSize && y1 >= 0 && y1 < fieldSize)
+                found &= (board.get(y1 * fieldSize + x1) == DOT_EMPTY);
+            x1 = x + 1;
+            y1 = y + 1;
+            if (x1 >= 0 && x1 < fieldSize && y1 >= 0 && y1 < fieldSize)
+                found &= (board.get(y1 * fieldSize + x1) == DOT_EMPTY);
+            x1 = x;
+            y1 = y + 1;
+            if (x1 >= 0 && x1 < fieldSize && y1 >= 0 && y1 < fieldSize)
+                found &= (board.get(y1 * fieldSize + x1) == DOT_EMPTY);
+            x1 = x - 1;
+            y1 = y + 1;
+            if (x1 >= 0 && x1 < fieldSize && y1 >= 0 && y1 < fieldSize)
+                found &= (board.get(y1 * fieldSize + x1) == DOT_EMPTY);
+            x1 = x - 1;
+            y1 = y;
+            if (x1 >= 0 && x1 < fieldSize && y1 >= 0 && y1 < fieldSize)
+                found &= (board.get(y1 * fieldSize + x1) == DOT_EMPTY);
+            if (found)
+                list.add(i);
+        }
+        for (int i : list)
+            board.set(i, DOT_MASK);
+        return list;
+    }
+
+    private void unmaskFreeCells(ArrayList<Integer> list) {
+        for (int i : list)
+            board.set(i, DOT_EMPTY);
+    }
+
+    /**
+     * вспомогательный класс
+     */
+    class Move {
+        int score;
+        int index;
+
+        Move(int score, int index) {
+            this.score = score;
+            this.index = index;
+        }
+
+        Move() {
+            this(0,0);
+        }
+    }
+
+    private Move MiniMax(ArrayList<Character> newBoard, char dot) {
+        // доступные клетки
+        ArrayList<Integer> availSpots = getFreeCellsIndexes(newBoard);
+
+        // проверка результата расчета
+        if (isWinCombination(newBoard, dotUser)) {
+            return new Move(-10, 0);
+        } else if (isWinCombination(newBoard, dotAi)) {
+            return new Move(10, 0);
+        } else if (availSpots.size() == 0)
+            return new Move();
+
+        // расчет ходов
+        ArrayList<Move> moves = new ArrayList<>();
+
+        // расчет очков по всем доступным клеткам
+        for (Integer item : availSpots) {
+            // создадим объект для хода
+//            Move move = new Move(0, item);
+
+            // сделаем ход за текущего игрока
+            newBoard.set(item, dot);
+
+            // рассчитаем очки
+            Move move = ((dot == dotAi) ? MiniMax(newBoard, dotUser) : MiniMax(newBoard, dotAi));
+            move.index = item;
+
+            // очистим клетку и сохраним результат
+            newBoard.set(item, DOT_EMPTY);
+            moves.add(move);
+        }
+
+        // выбор наилучшего хода
+        Move bestMove = new Move();
+        if (dot == dotAi) {
+            bestMove.score = -10000;
+            for (Move move : moves) {
+                if (move.score > bestMove.score) {
+                    bestMove.score = move.score;
+                    bestMove.index = move.index;
+                }
+            }
+        } else {
+            bestMove.score = 10000;
+            for (Move move : moves) {
+                if (move.score < bestMove.score) {
+                    bestMove.score = move.score;
+                    bestMove.index = move.index;
+                }
+            }
+        }
+
+        return bestMove;
+    }
+
+    /**
+     * @param board расклад, для которого идет расчет
+     * @return ArrayList
+     */
+    private ArrayList<Integer> getFreeCellsIndexes(ArrayList<Character> board) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < board.size(); i++)
+            if (board.get(i) == DOT_EMPTY)
+                list.add(i);
+        return list;
+    }
+
 }
--- src/main/java/KrestikiNoliki.java
+++ src/main/java/KrestikiNoliki.java
@@ -1,38 +1,86 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class KrestikiNoliki {
     private GameLogic gameLogic;
-    Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         new KrestikiNoliki();
     }
 
     public KrestikiNoliki() {
-        System.out.println("Игра крестики-нолики!");
+        try {
+            System.out.println("Игра крестики-нолики!");
 
-        // инициализация поля
-        int width = userInput("Введите ширину поля (3..10): ", 3, 10);
-        int height = userInput("Введите высоту поля (3..10): ", 3, 10);
-        int winLength = userInput("Введите длину выигрышной комбинации: ", 3, Math.max(width, height));
-        gameLogic = new GameLogic(width, height, winLength);
+            // инициализация поля
+            int size = userInput("Введите размер поля", 3, 9);
+            int winLength = userInput("Введите длину выигрышной комбинации", 3, size);
+            gameLogic = new GameLogic(size, winLength);
 
-        // чем будет играть юзер
-        int userDot = userInput("Вы будете играть 'X'(0) или 'O'(1) (введите 0 или 1): ", 0, 1);
-        if (userDot == 0) {
-            gameLogic.setDotUser('X');
-            gameLogic.setDotAi('O');
-        } else {
-            gameLogic.setDotUser('O');
-            gameLogic.setDotAi('X');
+            // чем будет играть юзер
+            int userDot = userInput("Вы будете играть 'X'(0) или 'O'(1)", 0, 1);
+            if (userDot == 0) {
+                gameLogic.setDotUser('X');
+                gameLogic.setDotAi('O');
+            } else {
+                gameLogic.setDotUser('O');
+                gameLogic.setDotAi('X');
+            }
+
+            // игровой цикл
+            printBoard();
+            while (true) {
+                humanTurn(); // ход игрока
+                if (gameLogic.isPlayerWin()) {
+                    printBoard();
+                    System.out.println("Вы выиграли!!!");
+                    break;
+                }
+                // поле заполнено?
+                if (gameLogic.isBoardFilled()) {
+                    printBoard();
+                    System.out.println("Игра окончилась ничьей.");
+                    break;
+                }
+                aiTurn(); // ход ИИ
+                if (gameLogic.isAiWin()) {
+                    printBoard();
+                    System.out.println("Вы проиграли!!!");
+                    break;
+                }
+                // поле заполнено?
+                if (gameLogic.isBoardFilled()) {
+                    printBoard();
+                    System.out.println("Игра окончилась ничьей.");
+                    break;
+                }
+                printBoard();
+            }
+        } catch (Exception e) {
+            System.out.println("Что-то пошло не так: " + e.getMessage());
         }
+    }
 
-        // печатаем поле
-        printMap();
+    /**
+     * запрос хода ИИ
+     */
+    private void aiTurn() {
+        gameLogic.makeAiTurn();
+    }
 
-        System.out.println("End game!");
+    /**
+     * запрос хода игрока
+     */
+    private void humanTurn() {
+        int x, y;
+        do {
+            y = userInput("Ваш ход, строка", 1, gameLogic.getFieldHeight());
+            x = userInput("Ваш ход, столбец", 1, gameLogic.getFieldSize());
+            x--;
+            y--;
+        } while (!gameLogic.makeHumanTurn(x, y));
     }
 
     /**
@@ -44,10 +92,11 @@ public class KrestikiNoliki {
      * @return int результат
      */
     private int userInput(String message, int min, int max) {
-        int chislo = 0;
+        String fullmessage = String.format("%s (%d..%d): ", message, min, max);
+        int chislo;
         while (true) {
             try {
-                System.out.println(message);
+                System.out.println(fullmessage);
                 chislo = scanner.nextInt();
                 if (chislo < min) {
                     System.out.println("Слишком маленькое число");
@@ -65,10 +114,31 @@ public class KrestikiNoliki {
     }
 
     /**
-     * вывод игрового поля
+     * вывод игрового поля вида
+     * | 1 2 3
+     * --------
+     * 1| . . .
+     * 2| . . .
+     * 3| . . .
      */
-    private void printMap() {
-        int map[][] = gameLogic.getMap();
-        System.out.println(Arrays.deepToString(map));
+    private void printBoard() {
+        ArrayList<Character> board = gameLogic.getBoard();
+        int width = gameLogic.getFieldSize();
+        int height = gameLogic.getFieldHeight();
+        System.out.print(" |");
+        for (int i = 1; i <= width; i++)
+            System.out.print(" " + i);
+        System.out.println();
+        for (int i = 0; i <= width; i++)
+            System.out.print("--");
+        for (int i = 0; i < board.size(); i++) {
+            if (i % width == 0) {
+                // новая строка
+                System.out.println();
+                System.out.print((i / width + 1) + "|");
+            }
+            System.out.print(" " + board.get(i));
+        }
+        System.out.println();
     }
 }
